4c784a96fdad74547ece122ebd00dafcccd32751
iorn <wmt.tkn.121@gmail.com>
2021-12-07 17:34:42 +0900

    add new thumbnail

--- js/index.js
+++ js/index.js
@@ -16,7 +16,10 @@ new Vue({
             {title: "VRChat", category: 'Enjoy', image: "../images/thumb_vrchat.jpg", url:"https://vrchat.com/home/user/usr_0c1fec8f-a643-40f2-a884-80b5915a53d7"},
             {title: "GasshoZukuri VR", category: 'Create', image: "../images/thumb_gasshozukuri.jpg", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
             {title: "Biography", category: 'Others', image: "../images/thumb_biography.jpg", url:""},
-            {title: "Photograph", category: 'Create', image: "../images/thumb_photograph.jpg", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"}
+            {title: "Photograph", category: 'Create', image: "../images/thumb_photograph.jpg", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
+            {title: "Movie", category: 'Create', image: "../images/thumb_movie.png", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
+            {title: "Windmill", category: 'Create', image: "../images/thumb_windmill.jpg", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
+            {title: "ColorScheme Converter", category: 'Create', image: "../images/thumb_colorscheme.png", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"}
         ],
         shuffle_photo: []
         }
