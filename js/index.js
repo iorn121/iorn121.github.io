60aebf487cee623c407b2da6734ec225d9557ab6
iorn <wmt.tkn.121@gmail.com>
2021-12-09 15:47:58 +0900

    フォント追加と自己紹介ページ追加

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ new Vue({
             {title: "AtCoder", category: 'Study', image: "../images/thumb_atcoder.png", url:"https://atcoder.jp/users/Iorn"},
             {title: "VRChat", category: 'Enjoy', image: "../images/thumb_vrchat.jpg", url:"https://vrchat.com/home/user/usr_0c1fec8f-a643-40f2-a884-80b5915a53d7"},
             {title: "GasshoZukuri VR", category: 'Create', image: "../images/thumb_gasshozukuriVR.jpg", url:"./articles/gasshozukuriVR.html"},
-            {title: "Biography", category: 'Others', image: "../images/thumb_biography.jpg", url:""},
+            {title: "Biography", category: 'Others', image: "../images/thumb_biography.jpg", url:"./articles/biography.html"},
             {title: "Photograph", category: 'Create', image: "../images/thumb_photograph.jpg", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
             {title: "Movie", category: 'Create', image: "../images/thumb_movie.png", url:"https://www.youtube.com/watch?v=qT6ygPjV_F0&t=0s"},
             {title: "Savonius Windmill", category: 'Create', image: "../images/thumb_windmill.jpg", url:"./articles/windmill.html"},
